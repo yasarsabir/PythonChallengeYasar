a7c9a50dfaeaacb8f2c9c9435aceade92d9692bd
yasarsabir <130310384+yasarsabir@users.noreply.github.com>
2023-05-15 12:53:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,66 +4,75 @@ import csv
 import pandas as pd
 
 #assign file to read
-csvpath = os.path.join('Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'election_data.csv')
 
 #set variables
 count = 0
-net_change_list = []
-Maximum_profit = 0
-month_max_profit =""
-Maximum_loss = 0
-month_max_loss = ""
-total_profit_loss = 0
+candidate_list = []
+Votes_for_Charles = 0
+Votes_for_Diana = 0
+Votes_for_Raymon = 0
+
 
 #open the file and read the data
 with open(csvpath) as text: 
-    data = csv.reader(text)
-#discard hearder
+    data = csv.reader(text, delimiter=",")
+
+#discard header
     header = next(data)
     first_row = next(data)
-    previous_net = float(first_row[1])
 
-#1) COUNT the number of months using a for loop 
+#1) COUNT the total number of votes, that is the number of rows in column 1 
     for row in data:
-        count  += 1
-        total_net = int(row[1])
-        
-        #note count += 1 is same as count = count + 1
-   #redundant code to check the data types of the numbers given in the csv file
-        #print (total_net) 
-        #print (previous_net)
-        #print(type(previous_net))
-        #print(type(total_net))
-        # they were both strings
+            count  += 1
 
-        net_change = total_net - previous_net
-        previous_net = total_net
-        net_change_list += [net_change]
-        total_profit_loss += int(row[1])
-        if net_change > Maximum_profit :
-            Maximum_profit = net_change
-            month_max_profit = row[0]
-        if net_change < Maximum_loss:
-            Maximum_loss = net_change
-            month_max_loss = row [0]
+# 2) Complete the candidate list, an empty candidate list has already been assigned above.
+#assign a variable to look through Column C in the database
+data_column = row[2]
 
-average_change = sum(net_change_list)/len(net_change_list)
 
-#need to fix the average change value 
+# Iterate over each item in the data column
+for item in data_column:
+    # Check if the item is not already in the candidate_list
+    if item not in candidate_list:
+        # Add the item to the candidate_list
+        candidate_list.append(item)
 
-# Print all the information
-analysis = (f'''Financial Analysis
----------------------------- 
-Total Months:   {int(count) + 1} 
-Total: $  {total_profit_loss}
-Average Change: ${average_change:.2f}
-Greatest Increase in Profits: {month_max_profit}  ($ {int(Maximum_profit)} ) 
-Greatest Decrease in Profits: {month_max_loss}  ($ {int(Maximum_loss)} ) '''
-)
-#print analysis
+# Print the unique strings
+print(candidate_list)
+#for row[2] in data:
+         #if candidate in row[2] != previous_row   :
+            #candidate_list.append(row[2])
+
+# 3) Cacluate the total number of candidates running by finding all the unique values in Column C not including the header.
+    
+if row[2] == "Charles Casper Stockham":
+        Votes_for_Charles  += 1
+elif row[2] == "Diana DeGette":
+        Votes_for_Diana    += 1
+elif row[2] == "Raymon Anthony Doane":
+        Votes_for_Raymon   += 1
+         
+#3) Calculate the percentage of votes each candidate won AND the absolute value 
+Percentage_votes_Charles = (Votes_for_Charles/count)*100
+Percentage_votes_Diana = (Votes_for_Diana/count)*100
+Percentage_votes_Raymon = (Votes_for_Raymon/count)*100
+
+analysis = (f'''
+Election Results
+-------------------------
+Total Votes:  {count}    
+-------------------------
+Charles Casper Stockham: {Percentage_votes_Charles}% ({Votes_for_Charles})
+Diana DeGette: {Percentage_votes_Diana}% ({Votes_for_Diana}))
+Raymon Anthony Doane: {Percentage_votes_Raymon}% ({Votes_for_Raymon}))
+-------------------------
+Winner: Diana DeGette
+-------------------------''')
+#print the analysis to the terminal
 print(analysis)
-# Export analysis to analysis.py.
-#specify the output file
+
+#write the analysis to the text file
 output_path = os.path.join('analysis.txt')
 with open(output_path, 'w') as textfile:
     textfile.write(analysis)
